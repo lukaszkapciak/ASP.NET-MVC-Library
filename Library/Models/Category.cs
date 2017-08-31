5f7ced0056ea412e6e44c267e624c7539b46dbb7
lukaszkapciak <lukaszkapciakup@gmail.com>
2017-08-31 19:52:28 +0200

    CategoryModel Update [Required attr. on Name property]

--- Library/Models/Category.cs
+++ Library/Models/Category.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Library.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+        [Required]
         public string Name { get; set; }
         public virtual ICollection<Book> Books { get; set; }
     }
