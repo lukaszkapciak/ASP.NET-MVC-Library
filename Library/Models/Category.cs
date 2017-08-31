3c425a6dfdad39474172a15adcf6d0e709eed600
lukaszkapciak <lukaszkapciakup@gmail.com>
2017-08-31 20:04:39 +0200

    model up

--- Library/Models/Category.cs
+++ Library/Models/Category.cs
@@ -5,6 +5,7 @@ namespace Library.Models
 {
     public class Category
     {
+        [Required]
         public int CategoryId { get; set; }
         [Required]
         public string Name { get; set; }
